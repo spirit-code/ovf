508be08109e3fc82fa86e7bf3ab565a0bc085442
Gideon <g.mueller@fz-juelich.de>
2018-06-29 17:30:12 +0200

    Added Fortan build and tests to CMake.

---
+++ fortran/test/simple.f90
@@ -0,0 +1,10 @@
+
+program main
+use, intrinsic :: iso_c_binding
+use ovf
+
+    write (*,*) "---------- fortran test of ovf ----------"
+
+    write (*,*) "------ finished fortran test of ovf -----"
+
+end program main
