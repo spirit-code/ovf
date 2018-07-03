90458a4f823a820b35b837fd0430f36a3423988e
Gideon <g.mueller@fz-juelich.de>
2018-07-03 22:40:08 +0200

    Removed unneeded script.

--- run.sh
+++
@@ -1,7 +0,0 @@
-cd build
-make
-cd ..
-gfortran -c fortran/ovf.f90 -o f_ovf.o
-gfortran -c fortran/test/simple.f90 -o f_simple.o
-gfortran f_simple.o f_ovf.o build/libovf_static.a -o simple.x  -lstdc++   
-./simple.x
